0489d32e07f51a128a7b3759dc8e14bda0295297
ConnorFieldUser <conquest.field@gmail.com>
2016-11-04 09:46:47 -0400

    commit

--- childcare_checkin/checkin/views.py
+++ childcare_checkin/checkin/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views.generic import TemplateView
+
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+from django.views.generic.edit import CreateView
 
 # Create your views here.
+
+
+class UserCreateView(CreateView):
+    models = User
+    form_class = UserCreationForm
+    success_url = "/"
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
