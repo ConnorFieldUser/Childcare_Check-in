43a1406e5742519264b97517ed4728fac842501d
ConnorFieldUser <conquest.field@gmail.com>
2016-11-04 08:57:46 -0400

    commit

--- childcare_checkin/checkin/models.py
+++ childcare_checkin/checkin/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+from django.contrib.auth.models import User
+
+
 # Create your models here.
+
+
+class Check(models.Model):
+
+    checkin = models.DateTimeField(auto_now=True)
+    checkout = models.DateTimeField(auto_now=True)
+    hours = models.IntegerField(null=True, blank=True)
+
+
+class Child(models.Model):
+
+    first = models.CharField(max_length=10)
+    last = models.CharField(max_length=10)
+    pin = models.IntegerField(unique=True)
+    parent = models.ForeignKey(User)
+
+
+ACCESS_LEVELS = [
+    ('s', 'Staff'),
+    ('p', 'Parent'),
+]
+
+
+class Profile(models.Model):
+    user = models.OneToOneField('auth.User')
+    access_level = models.CharField(max_length=1, choices=ACCESS_LEVELS)
+
+    def __str__(self):
+        return self.access_level
+
+    @property
+    def owner(self):
+        return self.access_level == 's'
+
+    @property
+    def parent(self):
+        return self.access_level == 'p'
+
+
+@receiver(post_save, sender="auth.User")
+def create_user_profile(**kwargs):
+    created = kwargs.get('created')
+    instance = kwargs.get('instance')
+    if created:
+        Profile.objects.create(user=instance)
--- childcare_checkin/childcare_checkin/settings.py
+++ childcare_checkin/childcare_checkin/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'checkin'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'childcare_checkin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
